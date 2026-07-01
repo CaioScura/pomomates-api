3539ec4f4b22fed8458b1afe037654ff1bc76ac4
CaioScura <caioscura02@gmail.com>
2026-07-01 16:56:59 -0300

    start api pomomates

---
+++ src/main/java/com/pomomates/pomomates_api/PomomatesApiApplication.java
@@ -0,0 +1,13 @@
+package com.pomomates.pomomates_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomomatesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomomatesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pomomates/pomomates_api/PomomatesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomomates.pomomates_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomomatesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
